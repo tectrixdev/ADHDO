42a1a7bc22fb32fb235545d4ab39629631e2fc18
TecTrix <joranhennion@duck.com>
2025-04-23 20:53:42 +0200

    remove files on dev because I'm remaking

--- src/app/components/ColorAlgorithm.js
+++
@@ -1,5 +0,0 @@
-export default function getColor(value, opacity) {
-  // color algorithm i totally not stole from stack overflow, be honest you would too
-  var hue = (value * 120).toString(10); // no idea what this does lmao ;)
-  return ["hsla(", hue, ",100%,20%, ", opacity, ")"].join(""); // changed the values here though so i'd call it original
-}
