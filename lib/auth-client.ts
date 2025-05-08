61278d7a6fe33de9fa55d4965a49b86b79c12242
Joran Hennion <joranhennion@duck.com>
2025-05-08 21:51:40 +0200

    add some auth that currently doesn't work but that's not important ahh

---
+++ lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+export const authClient = createAuthClient({});
+
+const signIn = async () => {
+  const data = await authClient.signIn.social({
+    provider: "github",
+  });
+};
